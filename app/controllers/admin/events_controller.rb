519574b1ab4514874d2c09b144126c3178e94e70
yagitatsu0326 <yagitatsuhiro0326@gmail.com>
2021-01-23 07:04:52 +0000

    admin event,school,staff

---
+++ app/controllers/admin/events_controller.rb
@@ -0,0 +1,50 @@
+class Admin::EventsController < ApplicationController
+
+	def new
+		@event =Event.new
+	end
+
+	def create
+		@event =Event.new(event_params)
+		@event.admin_id = current_admin.id
+		if @event.save
+			redirect_to admin_events_path
+		else
+			render 'new'
+		end
+	end
+
+	def index
+		@events =Event.all
+	end
+
+	def show
+		@event = Event.find(params[:id])
+	end
+
+	def edit
+		@event = Event.find(params[:id])
+	end
+
+	def update
+		@event = Event.find(params[:id])
+		@event.admin_id = current_admin.id
+		if @event.update(event_params)
+			redirect_to admin_events_path
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@event = Event.fing(params[:id])
+		@event.destroy
+		redirect_to admin_events_path
+	end
+
+	private
+	def event_params
+		params.require(:event).permit(:title,:body,:place,:start_time,:end_time)
+	end
+
+end
