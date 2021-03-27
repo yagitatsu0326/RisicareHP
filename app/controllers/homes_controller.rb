2d343352f5d0bc72b4d0ed8b614ec64949907ec3
yagitatsu0326 <yagitatsuhiro0326@gmail.com>
2021-03-27 07:43:56 +0000

    risicare hp

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,10 @@
 class HomesController < ApplicationController
 	def top
 		@informations = Information.order(created_at: :desc).limit(4).order(created_at: :desc)
+        @events = Event.where("start_time > ?",Date.today).order(start_time: :asc).limit(7)
+
 	end
 
+	def about
+	end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,10 @@ class Event < ApplicationRecord
 
 	belongs_to :admin
 
+	validates :title, presence: true, length: { maximum: 255 }
+	validates :body, presence: true, length: { maximum: 255 }
+	validates :place, presence: true
+	validates :start_time, presence: true
+	validates :end_time, presence: true
+
 end
