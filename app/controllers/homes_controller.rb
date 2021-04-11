0b20bf6d9b9ad64f3782323a4533ea5d6a992045
yagitatsu0326 <yagitatsuhiro0326@gmail.com>
2021-04-11 14:29:06 +0000

    risicareHP

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
 	def top
-		@informations = Information.order(created_at: :desc).limit(4).order(created_at: :desc)
+		@informations = Information.order(created_at: :desc).limit(6).order(created_at: :desc)
         @events = Event.where("start_time > ?",Date.today).order(start_time: :asc).limit(7)
 
 	end
